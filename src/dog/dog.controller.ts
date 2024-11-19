767741709c7280c7481f2628376f935653e59e19
javlonumrzokov <jackumrzokov@gmail.com>
2024-11-19 11:38:28 +0500

    BRR: Initial Setup

---
+++ src/dog/dog.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { DogService } from './dog.service';
+
+@Controller('dog')
+export class DogController {
+  constructor(private readonly dogService: DogService) {}
+
+  @Get()
+  public getHello(): string {
+    return this.dogService.getHello();
+  }
+
+  @Get('greet/:id/')
+  public intoduce(@Param() params: any, @Query() query: any): string {
+    console.log('params:', params);
+    console.log('query:', query);
+    return this.dogService.intoduce();
+  }
+}
